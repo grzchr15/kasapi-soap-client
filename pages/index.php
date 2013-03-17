1065a30f55854996da199f0f32196af8c692723e
Christian Bretterhofer <Christian.Bretterhofer@gmail.com>
2013-03-17 21:17:46 +0100

    Added WIKI

---
+++ pages/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "autoconfig for thunderbird"."</BR>";
+
+echo "This Software works for (PHP 5 >= 5.3.0)"."</BR>";
+echo 'Current  PHP Version is ' . phpversion()."</BR>";
+
+echo "see http://all-inkl.com/wichtig/ how to change php version"."</BR>";
+
+?>
